e72dd037f05bd860bd10f73b12dd3c30c40a4550
victory-nwawunze <victorynwawunze@gmail.com>
2022-05-23 15:25:33 -0700

    creating routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 /** @format */
 
+import React from "react";
 import "./App.css";
 import Header from "./containers/Header";
 import ProductListing from "./containers/ProductListing";
-
+import { Route, Routes, Navigate } from "react-router-dom";
+import ProductDetail from "./containers/ProductDetail";
 function App() {
   return (
-    <div>
+    <React.Fragment>
       <Header />
-      <ProductListing />
-    </div>
+      <Routes>
+        <Route path={"/"} element={<Navigate to="fakeshop/list" />} />
+        <Route path={"/fakeshop/list"} element={<ProductListing />} />
+        <Route path={"/product/:productId"} element={<ProductDetail />} />
+        <Route>404 not found</Route>
+      </Routes>
+    </React.Fragment>
   );
 }
 
--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -2,8 +2,12 @@
 
 import React from "react";
 
-function ProductListing() {
-  return <div>ProductListing</div>;
-}
+const ProductListing = () => {
+  return (
+    <div>
+      <h1>ProductListing</h1>
+    </div>
+  );
+};
 
 export default ProductListing;
